3ddc7f4538c4b1c4fa953ecae78af27233712dd5
Matthew Tang <46803888+MatttTang@users.noreply.github.com>
2020-03-13 01:52:43 -0700

    Add files via upload
    Working Code, creates clique. Need to allow for remainders and couples

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.MultiGraph;
 import org.graphstream.graph.implementations.SingleGraph;
 
 import java.nio.file.Files;
@@ -25,10 +26,16 @@ public class Main {
         String[] names = data.split("\n", 0);
 
         // Create a graph
-        Graph graph = new SingleGraph("Tutorial");
+        Graph graph = new MultiGraph("Tutorial");
 
         // Create a vector filled with each name
         Vector<People> list = new Vector<People>();
+        graph.display();
+
+        // Makes sure names are without the end line character
+        for (int i = 0; i < names.length; i++) {
+            names[i] = names[i].substring(0, names[i].length() - 1);
+        }
 
         int totWeight = 0;
         // Add in each person to the vector while adding them into the graph as nodes
@@ -36,13 +43,13 @@ public class Main {
             list.add(new People(names[i]));
             totWeight += list.elementAt(i).weight;
             graph.addNode(names[i]);
-            graph.display();
         }
 
         // Create an adjacency matrix for the name list
         boolean[][] adj = new boolean[names.length][names.length];
 
-        for (int i = 0; i < names.length; i++){
+        // Initialize nodes so that they cannot visit themselves, do it by initialize their spot in the matrix to true
+        for (int i = 0; i < names.length; i++) {
             adj[i][i] = true;
         }
 
@@ -52,64 +59,62 @@ public class Main {
 
         int connection = names.length * (names.length - 1);
         int counter = 0;
+        int iterationNum = 1;
 
-        int i = 0;
-
+        // Create a vector of groups to hold all groups for this iteration
         Vector<Group> gList = new Vector<Group>();
-        while (i < names.length){
-            // Iterate through the column of visits to person i
-            for (int j = 0; j < names.length; j++){
-                // If a person has not visited i, and they are free, add them into a group with i
-                if (adj[j][i] == false && list.get(j).free == true){
-                    // If i is free, create a group with them in it
-                    if (list.get(i).free){
-                        gList.add(new Group(groupSize));
-                        gList.lastElement().Add(list.get(i));
-                        list.get(i).free = false;
-                    }
-                    if (gList.lastElement().Check(list.get(j))){
-                        gList.lastElement().Add(list.get(j));
-                        list.get(j).free = false;
-                        graph.addEdge("a" + i + j, list.get(i).name, list.get(j).name);
-                        graph.display();
-                    }
-                }
-            }
-            i++;
-        }
 
-
-
-        // While our connections are less than what we need, keep in the loop
-        /*
-        while (actual < connections){
-            // Create an iterator through the list of people
-            gActual = 0;
-            ListIterator<People> iterator = (ListIterator<People>) list.iterator();
-            ListIterator<People> holder = iterator;
-            Vector<Group> gList = new Vector<Group>();
-            gList.add(new Group(perG));
-            // Iterate through the list, and if the weight fits within the group and the person is free, include them in the group
-            while (iterator.hasNext()){
-                People temp = iterator.next();
-                // If the group does not hold remainders, add it into list of groups
-                if (gList.lastElement().isFull()) {
-                    if (gActual + 1 < groups) {
-                        gList.add(new Group(perG));
-                    }
-                }
-                // If the person being checked is free and fits into the group, add them to the group
-                    if (temp.free && gList.lastElement().Check(temp)){
-                        gList.lastElement().Add(temp);
-                        // If the current group is full, then print it out
-                        if (gList.lastElement().isFull()){
-                            System.out.print("\n");
+        // While we do not have n * (n - 1) connections
+        while (counter < connection) {
+            // Iterate through the hosts, if any of the hosts are missing a visitor, then they host a group
+            for (int i = 0; i < names.length; i++) {
+                // Iterate through the column to see visitors of person i
+                for (int j = 0; j < names.length; j++) {
+                    // If a person has not visited i, and they are free, add them into a group with i
+                    if (adj[j][i] == false && list.get(j).free == true) {
+                        // If i is free, create a group with them in it
+                        if (list.get(i).free) {
+                            gList.add(new Group(groupSize));
+                            gList.lastElement().Add(list.get(i));
+                            list.get(i).free = false;
+                        }
+                        // Check if the person to be added can fit into the group, if they can add them, and create connection
+                        if (gList.lastElement().Check(list.get(j))) {
+                            gList.lastElement().Add(list.get(j));
+                            adj[j][i] = true;
+                            counter++;
+                            list.get(j).free = false;
+                            graph.addEdge("a" + counter , list.get(i).name, list.get(j).name);
                         }
                     }
-                actual = 42;
+                }
+                // If this host needs no more visitors, the move on
+                if (!gList.isEmpty()) {
+                    // If the current group is not full, the host needs to have guests they have already had before
+                    if (gList.lastElement().isFull() == false) {
+                        for (int t = 0; t < names.length; t++) {
+                            if (list.get(t).free == true && t != i) {
+                                gList.lastElement().Add(list.get(t));
+                                list.get(t).free = false;
+                            }
+                            if (gList.lastElement().isFull()) {
+                                break;
+                            }
+                        }
+                    }
+                }
             }
+            // Change everyone's availability to true
+            for (int z = 0; z < names.length; z++){
+                list.get(z).free = true;
+            }
+            for (int q = 0; q < gList.size(); q++){
+                System.out.print(iterationNum);
+                System.out.println(gList.get(q).occupants);
+            }
+            // Clear the groups for the iteration
+            gList.clear();
+            iterationNum++;
         }
-    }
-    */
     }
 }
