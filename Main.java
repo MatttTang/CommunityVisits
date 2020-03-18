17aa2c96d459da15ea8a984aa9ed9e0ef7299965
Matthew Tang <46803888+MatttTang@users.noreply.github.com>
2020-03-18 02:11:58 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,25 +2,33 @@ package com.company;
 
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.MultiGraph;
-import org.graphstream.graph.implementations.SingleGraph;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Vector;
+import java.util.*;
 
 public class Main {
+    // Function to read in file as a string, parse through it later
     public static String readFile(String fileName) throws Exception {
         String data = "";
-        data = new String(Files.readAllBytes(Paths.get(fileName)));
-        return data;
+        File f = new File(fileName);
+        // Error check if file exists
+        if (f.exists() && !f.isDirectory()) {
+            data = new String(Files.readAllBytes(Paths.get(fileName)));
+            return data;
+        }else{
+            System.out.println("File does not exist");
+            System.exit(0);
+        }
+        return fileName;
     }
 
     public static void main(String args[]) throws Exception {
         // Read in the file of names
-        String data = readFile("D:\\CommunityVisit\\src\\names.txt");
+        System.out.println("Enter File Name: ");
+        Scanner sfScan = new Scanner(System.in);
+        String data = readFile(sfScan.next());
 
         // Split the names by new line characters
         String[] names = data.split("\n", 0);
@@ -28,19 +36,31 @@ public class Main {
         // Create a graph
         Graph graph = new MultiGraph("Tutorial");
 
+        // Display Graph
+        graph.display();
+
         // Create a vector filled with each name
         Vector<People> list = new Vector<People>();
-        graph.display();
 
         // Makes sure names are without the end line character
         for (int i = 0; i < names.length; i++) {
             names[i] = names[i].substring(0, names[i].length() - 1);
         }
 
+        // Values to hold number of couples, number of singles, and number of total people
+        int singles = 0;
+        int couples = 0;
         int totWeight = 0;
+
         // Add in each person to the vector while adding them into the graph as nodes
         for (int i = 0; i < names.length; i++) {
             list.add(new People(names[i]));
+            if (list.lastElement().weight == 1){
+                singles++;
+            }else{
+                couples++;
+            }
+            // Read in total weight of people
             totWeight += list.elementAt(i).weight;
             graph.addNode(names[i]);
         }
@@ -53,12 +73,30 @@ public class Main {
             adj[i][i] = true;
         }
 
-        int groupSize = 4;
-        int groups = 2;
-        // Remainder
+        int groupSize;
+        int groups;
+        boolean valid = true;
+        Scanner myScan = new Scanner(System.in);
 
+        // Ensure that user input makes a valid group size and group number
+        do{
+            valid = true;
+            do{
+                System.out.println("Enter Group Number");
+                groups = myScan.nextInt();
+            } while (groups <= 0);
+            groupSize = totWeight / groups;
+            if (groupSize < 2 || groupSize > totWeight || (groupSize == 2 && couples > 0) || (groupSize == 3 && couples > 1) || (groupSize % 2 == 0 && singles == 1) || (groupSize % 2 == 1 && singles == 0)){
+                valid = false;
+            }
+        }while (!valid);
+        int rem = totWeight% groups;
+
+        // Number of total connections to be made will always be n * (n - 1), when we hit this number, we are done
         int connection = names.length * (names.length - 1);
         int counter = 0;
+
+        // Iterator to help print out the groups
         int iterationNum = 1;
 
         // Create a vector of groups to hold all groups for this iteration
@@ -66,6 +104,7 @@ public class Main {
 
         // While we do not have n * (n - 1) connections
         while (counter < connection) {
+            int remCount = rem;
             // Iterate through the hosts, if any of the hosts are missing a visitor, then they host a group
             for (int i = 0; i < names.length; i++) {
                 // Iterate through the column to see visitors of person i
@@ -74,8 +113,14 @@ public class Main {
                     if (adj[j][i] == false && list.get(j).free == true) {
                         // If i is free, create a group with them in it
                         if (list.get(i).free) {
-                            gList.add(new Group(groupSize));
-                            gList.lastElement().Add(list.get(i));
+                            // If there are remainders, make the group size 1 more
+                            if (remCount > 0){
+                                gList.add(new Group(groupSize + 1));
+                                remCount = remCount - 1;
+                            }else {
+                                gList.add(new Group(groupSize));
+                            }
+                            gList.lastElement().Add((list.get(i)));
                             list.get(i).free = false;
                         }
                         // Check if the person to be added can fit into the group, if they can add them, and create connection
@@ -93,7 +138,7 @@ public class Main {
                     // If the current group is not full, the host needs to have guests they have already had before
                     if (gList.lastElement().isFull() == false) {
                         for (int t = 0; t < names.length; t++) {
-                            if (list.get(t).free == true && t != i) {
+                            if (list.get(t).free == true && t != i && gList.lastElement().Check(list.get(t))) {
                                 gList.lastElement().Add(list.get(t));
                                 list.get(t).free = false;
                             }
@@ -104,14 +149,46 @@ public class Main {
                     }
                 }
             }
+            // Force number of groups to always be equal to group number
+            if (gList.size() != groups){
+                for (int q = 0; q < names.length; q++){
+                    for (int y = 0; y < names.length; y++){
+                        if (list.get(q).free && list.get(y).free){
+                            // If there are remainders, create a group size of 1 more
+                            if (remCount > 0){
+                                gList.add(new Group(groupSize + 1));
+                                remCount = remCount - 1;
+                            }else {
+                                gList.add(new Group(groupSize));
+                            }
+                            gList.lastElement().Add(list.get(q));
+                            list.get(q).free = false;
+                            list.get(y).free = false;
+                        }
+                        if (list.get(y).free){
+                            if (gList.lastElement().Check(list.get(y))){
+                                gList.lastElement().Add(list.get(y));
+                                list.get(y).free = false;
+                            }
+                        }
+                    }
+                }
+            }
             // Change everyone's availability to true
             for (int z = 0; z < names.length; z++){
                 list.get(z).free = true;
             }
+
+            // Print out each iteration
+            System.out.println("Iteration: " + iterationNum);
             for (int q = 0; q < gList.size(); q++){
-                System.out.print(iterationNum);
+                System.out.print("G" + iterationNum);
+                System.out.print(",");
+                System.out.print(q + 1);
+                System.out.print(":");
                 System.out.println(gList.get(q).occupants);
             }
+            System.out.print("\n");
             // Clear the groups for the iteration
             gList.clear();
             iterationNum++;
